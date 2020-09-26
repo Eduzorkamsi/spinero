a476e15ebe431f4080a025c31cd12502acd660ef
Harrison <fayvorharrison101@gmail.com>
2020-09-26 14:43:00 +0100

    implement cart integrations

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,3 +1,4 @@
 export * from "./user";
 export * from "./home";
 export * from "./product";
+export * from "./cart";
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import user from "./user";
 import home from "./home";
 import product from "./product";
+import cart from "./cart";
 
 const rootReducers = combineReducers({
   user,
   home,
-  product
+  product,
+  cart
 });
 
 export default rootReducers;
