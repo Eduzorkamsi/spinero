30b65b45c9f7827e30c27f2bb1eacb02ee45f3b5
Eduzor kamsi <40744869+Eduzorkamsi@users.noreply.github.com>
2020-10-02 17:41:40 +0100

    profile cart info

--- src/pages/ProfileCart/index.js
+++ src/pages/ProfileCart/index.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Footer from "../../components/Footer";
+import TopNav from "../../components/TopNav";
+import ProfileCartSidebar from "../ProfileCartSidebar";
 
 import "./style.css"
 
@@ -8,24 +11,180 @@ import "./style.css"
 const ProfileCart = () => {
     return (
         <>
-            <div className="profile_person_info">
-          
-                <div className="profile_person_info_details">
-                    <div></div>
-                    
-                    <div className="personal_information">
-                        <div className="profile_picture">
-                            <img src={require("../../assets/images/profileface.svg")} />
-
-                            <h5>Edit picture</h5>
+            <TopNav />
+            <div className="profile_cart">
+                <ProfileCartSidebar />
+                <div className="profile_cart_info">
+                    <div className="profile_cart_info_box1">
+                        <p>Cart</p>
+                        <img src={require("./../../assets/icons/minus.svg")} />
+                    </div>
+                    <div className="profile_cart_info_box2">
+                        <div className="profile_cart_box2">
+                            <div className="profile_cart_box21">
+                                <img src={require("./../../assets/images/girl.png")} />
+                            </div>
+                            <div className="profile_cart_box21b">
+                                <img src={require("./../../assets/icons/x.svg")} />
+                                <div className="profile_cart_inner1">
+                                    <h3>Women's Turtle Neck</h3>
+                                    <h4>$500</h4>
+                                </div>
+                                <div className="profile_cart_inner2">
+                                    <p>Size: S</p>
+                                    <p>Qty: 1</p>
+                                    <p >Color </p>
+                                    <p className="cart_color_box"></p>
+                                </div>
+                            </div>
                         </div>
-                        <div className="profile_box1">
-                            <p>Personal Information</p>
+
+                        <div className="profile_cart_box3">
+                            <div className="profile_cart_box22">
+                                <img src={require("./../../assets/images/girl.png")} />
+                            </div>
+                            <div className="profile_cart_box22b">
+                                <img src={require("./../../assets/icons/x.svg")} />
+                                <div className="profile_cart_inner1">
+                                    <h3>Women's Turtle Neck</h3>
+                                    <h4>$500</h4>
+                                </div>
+                                <div className="profile_cart_inner2">
+                                    <p>Size: S</p>
+                                    <p>Qty: 1</p>
+                                    <p >Color </p>
+                                    <p className="cart_color_box"></p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="profile_cart_info_box3">
+                        <h5>Checkout</h5>
+                        <h6>$1000</h6>
+                    </div>
+                    <div className="profile_cart_info_box4">
+                        <p>Wishlist</p>
+                        <img src={require("./../../assets/icons/minus.svg")} />
+                    </div>
+                    <div className="profile_cart_info_box5">
+                        <div className="box5a">
+                            <img src={require("./../../assets/images/girl.png")} />
+                            <div className="box5ab">
+                                <img src={require("./../../assets/icons/x.svg")} />
+                                <h3>Women's Turtle Neck</h3>
+                                <h4>$500</h4>
+                            </div>
+                        </div>
+                        <div className="box5b">
+                            <img src={require("./../../assets/images/girl.png")} />
+                            <div className="box5ab">
+                                <img src={require("./../../assets/icons/x.svg")} />
+                                <h3>Women's Turtle Neck</h3>
+                                <h4>$500</h4>
+                            </div>
                         </div>
                     </div>
+                    <div className="profile_cart_info_box6">
+                        <p>Pending deliveries</p>
+                        <img src={require("./../../assets/icons/minus.svg")} />
+                    </div>
+                    <div className="profile_cart_info_box7">
+                        <h5>Order no: 1090908675</h5>
+                        <div className="profile_cart_box2">
+                            <div className="profile_cart_box21">
+                                <img src={require("./../../assets/images/girl.png")} />
+                            </div>
+                            <div className="profile_cart_box21b">
+                                <img src={require("./../../assets/icons/x.svg")} />
+                                <div className="profile_cart_inner1">
+                                    <h3>Women's Turtle Neck</h3>
+                                    <h4>$500</h4>
+                                </div>
+                                <div className="profile_cart_inner2">
+                                    <p>Size: S</p>
+                                    <p>Qty: 1</p>
+                                    <p >Color </p>
+                                    <p className="cart_color_box"></p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="profile_cart_box3">
+                            <div className="profile_cart_box22">
+                                <img src={require("./../../assets/images/girl.png")} />
+                            </div>
+                            <div className="profile_cart_box22b">
+                                <img src={require("./../../assets/icons/x.svg")} />
+                                <div className="profile_cart_inner1">
+                                    <h3>Women's Turtle Neck</h3>
+                                    <h4>$500</h4>
+                                </div>
+                                <div className="profile_cart_inner2">
+                                    <p>Size: S</p>
+                                    <p>Qty: 1</p>
+                                    <p >Color </p>
+                                    <p className="cart_color_box"></p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="profile_cart_info_box8">
+                        <h5>View Shipping Details</h5>
+                        <h6>$1000</h6>
+                    </div>
+                    <div className="profile_cart_info_box9">
+                        <p>Delivered Items</p>
+                        <img src={require("./../../assets/icons/minus.svg")} />
+                    </div>
+                    <div className="profile_cart_info_box10">
+                        <h5>Order no: 1090908675</h5>
+                        <div className="profile_cart_box2">
+                            <div className="profile_cart_box21">
+                                <img src={require("./../../assets/images/girl.png")} />
+                            </div>
+                            <div className="profile_cart_box21b">
+                                <img src={require("./../../assets/icons/x.svg")} />
+                                <div className="profile_cart_inner1">
+                                    <h3>Women's Turtle Neck</h3>
+                                    <h4>$500</h4>
+                                </div>
+                                <div className="profile_cart_inner2">
+                                    <p>Size: S</p>
+                                    <p>Qty: 1</p>
+                                    <p >Color </p>
+                                    <p className="cart_color_box"></p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="profile_cart_box3">
+                            <div className="profile_cart_box22">
+                                <img src={require("./../../assets/images/girl.png")} />
+                            </div>
+                            <div className="profile_cart_box22b">
+                                <img src={require("./../../assets/icons/x.svg")} />
+                                <div className="profile_cart_inner1">
+                                    <h3>Women's Turtle Neck</h3>
+                                    <h4>$500</h4>
+                                </div>
+                                <div className="profile_cart_inner2">
+                                    <p>Size: S</p>
+                                    <p>Qty: 1</p>
+                                    <p >Color </p>
+                                    <p className="cart_color_box"></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="profile_cart_info_box11">
+                        <h5>View Shipping Details</h5>
+                        <h6>$1000</h6>
+                    </div>
                 </div>
-               
             </div>
+            <Footer />
         </>
 
     )
