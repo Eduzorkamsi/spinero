8df7f27da8c1fe15f8ea9ed354f8cc438da6ee0c
Harrison <fayvorharrison101@gmail.com>
2020-11-13 20:18:59 +0100

    fixes from testing

--- src/components/TopNav/overlay.js
+++ src/components/TopNav/overlay.js
@@ -2,63 +2,71 @@ import React, { useRef, useState } from "react";
 import SearchIcon from "../searchicon";
 import Button from "react-bootstrap/Button";
 import Overlay from "react-bootstrap/Overlay";
-import { nodeName } from "jquery";
+import { useHistory } from "react-router-dom";
 
 function SearchOverlay() {
+  const history = useHistory();
   const [show, setShow] = useState(false);
   const target = useRef(null);
+  const [searchText, setSearchText] = useState("");
 
   return (
     <>
-    <div className="overlay_search_mobile">
-    <Button variant="" ref={target} onClick={() => setShow(!show)}>
-        <SearchIcon />
-      </Button>
-      <Overlay target={target.current} show={show} placement="right">
-        {({ placement, arrowProps, show: _show, popper, ...props }) => (
-          <div
-            {...props}
-            style={{
-              backgroundColor: "white",
-              textAlign: "center",
-              width: "100%",
-              height: "100vh",
-              marginLeft: "-36%",
-              color: "green",
-              borderRadius: 3,
-              ...props.style,
-            }}
-          >
-            <button 
-            
-            style={{
-                
-              backgroundColor: "transparent",
-              border: "none",
-              marginRight: "21px"
-            }}
+      <div className="overlay_search_mobile">
+        <Button variant="" ref={target} onClick={() => setShow(!show)}>
+          <SearchIcon />
+        </Button>
+        <Overlay target={target.current} show={show} placement="right">
+          {({ placement, arrowProps, show: _show, popper, ...props }) => (
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                setShow(false);
+                history.push("/ProductSearch", { searchText });
+              }}
+            >
+              <div
+                {...props}
+                style={{
+                  backgroundColor: "white",
+                  textAlign: "center",
+                  width: "100%",
+                  height: "100vh",
+                  marginLeft: "-36%",
+                  color: "green",
+                  borderRadius: 3,
+                  ...props.style,
+                }}
+              >
+                <button
+                  style={{
+                    backgroundColor: "transparent",
+                    border: "none",
+                    marginRight: "21px"
+                  }}
+                  type="submit">
+                  <img
+                    alt="logo"
+                    className="search_logo"
+                    src={require("../../assets/icons/searchred.svg")}
+                  />
+                </button>
+                <input
+                  style={{
+                    width: "60%",
+                  }}
+                  className="searchInput"
+                  type="text"
+                  placeholder="What are you searching for?"
+                  value={searchText}
+                  onChange={(e) => setSearchText(e.target.value)}
+                />
+              </div>
+            </form>
+          )}
+        </Overlay>
+      </div>
 
-            type="submit">
-            
-              <img
-                alt="logo"
-                className="search_logo"
-                src={require("../../assets/icons/searchred.svg")}
-              />
-            </button>
-            <input
-            style={{
-              width:"60%",
-            }}
-              className="searchInput"
-              type="text"
-              placeholder="What are you searching for?"
-            />
-          </div>
-        )}
-      </Overlay>
-    </div>
-      
     </>
   );
 }
--- src/pages/Cart/indexmobile.js
+++ src/pages/Cart/indexmobile.js
@@ -1,68 +1,77 @@
 import React from "react";
 import "./stylemobile.css";
+import { useHistory } from "react-router-dom";
 
 
-export const CartMobile =() =>{
-    return(
-        <>
-        <div className="div2_mobile">
-         
-            <div className="media_mobile">
-              <img
-                src={""}
-                className="mr-3"
-                alt=""
-              />
-             
-              <div className="media-body-mobile">
+export const CartMobile = props => {
+  const history = useHistory();
+
+  return (
+    <>
+      <div className="div2_mobile">
+        {props.cart.products.map((product, i) => (
+          <div className="media_mobile">
+            <img
+              src={product.image}
+              className="mr-3"
+              alt="product"
+              onClick={() => {
+                history.push(`/ProductDetails/${product.id}`);
+              }}
+            />
+
+            <div className="media-body-mobile">
               <button
-                  type="button"
-                  className="button_times"
-                
-                >
-                  <i className="fas fa-times"></i>
-                </button>
+                type="button"
+                className="button_times"
+                onClick={() => {
+                  props.removeFromCart(product.id);
+                }}
+              >
+                <i className="fas fa-times"></i>
+              </button>
               <div className="media_text_mobile">
                 <button
                   type="button"
                   className="no-background no-border media_text_button"
-        
+                  onClick={() => {
+                    history.push(`/ProductDetails/${product.id}`);
+                  }}
                 >
-               Women's Turtle neck
+                  {product.name}
                 </button>
               </div>
-              <div className="cart_product_price_mobile">$</div>
+              <div className="cart_product_price_mobile">${product.price}</div>
               <div className="color_size_mobile">
-              <div className="li_text_mobile">S</div>
-                <div
-                  className="div4_mobile"
-                
-                ></div>
-               
+                <div className="li_text_mobile">{product.size}</div>
+                <div className="div4_mobile" style={{ background: `${product.color}` }}></div>
               </div>
-              
-                <div className="increment_indicator_mobile">
-                  <button
-                    type="button"
-                  >
-                    <i className="fas fa-plus"></i>
-                  </button>
-                  1
-                  <button
-                    type="button"
-                    className="no-border no-background"
-                 
-                  >
-                    <i className="fas fa-minus"></i>
-                  </button>
-                </div>
-               
-               
+
+              <div className="increment_indicator_mobile">
+                <button
+                  type="button"
+                  onClick={() => {
+                    props.addToCart(product);
+                  }}
+                >
+                  <i className="fas fa-plus"></i>
+                </button>
+                {product.quantity}
+                <button
+                  type="button"
+                  className="no-border no-background"
+                  onClick={() => {
+                    props.removeOneFromCart(product.id);
+                  }}
+                >
+                  <i className="fas fa-minus"></i>
+                </button>
               </div>
             </div>
-    
-        </div>
+          </div>
+        ))}
+      </div>
 
-        </>
-    )
+    </>
+  )
 }
